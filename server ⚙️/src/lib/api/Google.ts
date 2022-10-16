b3036f8baf27cedf74a08b4ad6870f9ce6b8b6e6
horusbou <bouqhamza@gmail.com>
2022-10-16 17:56:12 +0100

    adjusting the envirement Variables

--- server ⚙️/src/lib/api/Google.ts
+++ server ⚙️/src/lib/api/Google.ts
@@ -1,29 +1,31 @@
+import { config } from "dotenv"
 import { google } from "googleapis"
+config()
 
 const auth = new google.auth.OAuth2(
-  process.env.G_CLIENT_ID,
-  process.env.G_CLIENT_SECRET,
-  `${process.env.G_PUBLIC_URL}/login`
+    process.env.G_CLIENT_ID,
+    process.env.G_CLIENT_SECRET,
+    `${process.env.G_PUBLIC_URL}/login`
 )
 
 export const Google = {
-  authUrl: auth.generateAuthUrl({
-    access_type: "online",
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/userinfo.profile'
-    ]
-  }),
-  logIn: async (code: string) => {
-    const { tokens } = await auth.getToken(code);
+    authUrl: auth.generateAuthUrl({
+        access_type: "online",
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.email',
+            'https://www.googleapis.com/auth/userinfo.profile'
+        ]
+    }),
+    logIn: async (code: string) => {
+        const { tokens } = await auth.getToken(code);
 
-    auth.setCredentials(tokens)
+        auth.setCredentials(tokens)
 
-    const { data } = await google.people({ version: 'v1', auth }).people.get({
-      resourceName: 'people/me',
-      personFields: 'emailAddresses,names,photos'
-    })
-    return { user: data };
-  },
-  logOut: async (code: string) => { }
+        const { data } = await google.people({ version: 'v1', auth }).people.get({
+            resourceName: 'people/me',
+            personFields: 'emailAddresses,names,photos'
+        })
+        return { user: data };
+    },
+    logOut: async (code: string) => { }
 }
