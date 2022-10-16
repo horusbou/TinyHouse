cf05069af2b2a0d2f7640089ffd5f59fc06723da
horusbou <bouqhamza@gmail.com>
2022-10-16 17:54:58 +0100

    adding the Header of the App

---
+++ client 🎡/src/sections/AppHeader/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Logo from "./assets/logo.png"
+import { Link } from "react-router-dom";
+import { Layout } from "antd";
+import { MenuItems } from "./componenets";
+import { Viewer } from "../../lib/types";
+
+const { Header } = Layout;
+interface Props {
+    viewer: Viewer;
+    setViewer: (viewer: Viewer) => void;
+}
+
+export const AppHeader = ({ viewer, setViewer }: Props) => {
+    return (
+        <Header className="app-header">
+            <div className="app-header__logo-search-section">
+                <div className="app-header__logo">
+                    <Link to="/">
+                        <img src={Logo} alt="App logo" />
+                    </Link>
+                </div>
+            </div>
+            <div className="app-header__menu-section">
+                <MenuItems viewer={viewer} setViewer={setViewer} />
+            </div>
+        </Header>
+    )
+}
--- client 🎡/src/sections/index.ts
+++ client 🎡/src/sections/index.ts
@@ -1,3 +1,4 @@
+export * from './AppHeader'
 export * from "./Listings"
 export * from './NotFound'
 export * from './Listing'
