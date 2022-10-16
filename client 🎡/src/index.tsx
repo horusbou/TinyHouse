9eee5d325101c0ba5ba2a391d724dff89054147f
horusbou <bouqhamza@gmail.com>
2022-10-16 13:14:47 +0100

    generating the schema for Graphql server

--- client 🎡/src/index.tsx
+++ client 🎡/src/index.tsx
@@ -1,38 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Listings, Home, Host, Listing, NotFound, User } from "./sections"
+import { Layout } from 'antd';
+import { Listings, Home, Host, Listing, NotFound, User, Login } from "./sections"
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import './styles/index.css'
 
+
 const client = new ApolloClient({
-  uri: '/api',
-  cache: new InMemoryCache()
+    uri: '/api',
+    cache: new InMemoryCache()
 })
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/host" element={<Host />} />
-        <Route path="/listing/:id" element={<Listing />} />
-        <Route path="/listings/:location" element={<Listings title={""} />} />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <Router>
+            <Layout id="app">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/host" element={<Host />} />
+                    <Route path="/listing/:id" element={<Listing />} />
+                    <Route path="/listings/:location" element={<Listings title={""} />} />
+                    <Route path="/user/:id" element={<User />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Layout>
+        </Router>
+    )
 }
 
 
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
 );
