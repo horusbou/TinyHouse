288c1d12cfd779ee1b3c1fe77b4419e765efaa53
horusbou <bouqhamza@gmail.com>
2022-10-23 20:37:34 +0100

    adding the authorization and login via secured cookie

--- server ⚙️/src/graphql/resolvers/Viewer/index.ts
+++ server ⚙️/src/graphql/resolvers/Viewer/index.ts
@@ -1,14 +1,28 @@
+import { Response, Request } from "express"
 import { Google } from "../../../lib/api"
 import { Viewer, Database, User } from "../../../lib/types"
 import { LogInArgs } from "./types"
 import crypto from 'node:crypto'
 import { WithId } from 'mongodb'
 
-const logInViaGoogle = async (code: string, token: string, db: Database): Promise<WithId<User> | null> => {
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: true,
+  secure: process.env.NODE_ENV === "development" ? false : true,
+  signed: true
+}
+
+const logInViaGoogle = async (
+  code: string,
+  token: string,
+  db: Database,
+  res: Response): Promise<WithId<User> | null> => {
+  //console.log("via google")
   const { user } = await Google.logIn(code)
 
   if (!user)
     throw new Error("Google login error")
+
 
   // Names/Photos/Email Lists
   const userNamesList = user.names && user.names.length ? user.names : null
@@ -41,7 +55,7 @@ const logInViaGoogle = async (code: string, token: string, db: Database): Promis
   let viewer = updateRes.value
 
   if (!viewer) {
-    const insertResult = await db.users.insertOne({
+    db.users.insertOne({
       _id: userId,
       token,
       name: userName,
@@ -53,6 +67,25 @@ const logInViaGoogle = async (code: string, token: string, db: Database): Promis
     })
     viewer = await db.users.findOne({ _id: userId })
   }
+  res.cookie("viewer", userId, {
+    ...cookieOptions,
+    maxAge: 365 * 24 * 60 * 60 * 1000,
+
+  })
+  return viewer;
+}
+
+const logInViaCookie = async (token: string, db: Database, req: Request, res: Response): Promise<User | null> => {
+  const updateRes = await db.users.findOneAndUpdate(
+    { _id: req.signedCookies.viewer },
+    { $set: { token } },
+    { returnDocument: "after" }
+  );
+  let viewer = updateRes.value;
+  if (!viewer) {
+    res.clearCookie("viewer", cookieOptions)
+  }
+
   return viewer
 }
 
@@ -63,15 +96,17 @@ function authUrl(): string {
     throw new Error(`Failed to query Google Auth Url: ${err}`)
   }
 }
-async function logIn(_root: undefined, { input }: LogInArgs, { db }: { db: Database }) {
+
+async function logIn(_root: undefined, { input }: LogInArgs, { db, req, res }: { db: Database; req: Request; res: Response }) {
   try {
     //login in with google Auth url
 
     const code = input ? input.code : null
-    const token = crypto.randomBytes(16).toString()
-
-    const viewer: User | null = code ? await logInViaGoogle(code, token, db) : null
-
+    const token = crypto.randomBytes(16).toString('hex')
+    //console.log({ code, token })
+    const viewer: User | null = code
+      ? await logInViaGoogle(code, token, db, res)
+      : await logInViaCookie(token, db, req, res)
     if (!viewer)
       return { didRequest: true }
 
@@ -86,8 +121,9 @@ async function logIn(_root: undefined, { input }: LogInArgs, { db }: { db: Datab
     throw new Error(`Failed to log in: ${err}`)
   }
 }
-function logOut() {
+function logOut(_root: undefined, _args: {}, { res }: { res: Response }): Viewer {
   try {
+    res.clearCookie("viewer", cookieOptions)
     return { didRequest: true }
   } catch (error) {
     throw new Error(`Failed to log out: ${error}`)
--- server ⚙️/src/index.ts
+++ server ⚙️/src/index.ts
@@ -3,18 +3,26 @@ import express, { Application } from "express"
 import { ApolloServer } from "apollo-server-express"
 import { typeDefs, resolvers } from './graphql'
 import { connectDatabase } from './database'
+import cookieParser from "cookie-parser"
 
 
 const main = async (app: Application, port = process.env.PORT) => {
 
   const db = await connectDatabase()
-  const server = new ApolloServer({ typeDefs, resolvers, context: () => ({db}) }) //typedef: `` resolvers: {Query:{getUser()}}
+
+  //middleware
+  app.use(cookieParser(process.env.COOKIE_SECRET))
+  app.use(express.json())
+
+  const server = new ApolloServer({
+    typeDefs, resolvers, context: ({ req, res }) => ({
+      db, req, res
+    })
+  }) //typedef: `` resolvers: {Query:{getUser()}}
 
   await server.start();
   server.applyMiddleware({ app, path: '/api' })
 
-  //middleware
-  app.use(express.json())
 
 
   app.listen(port, () => console.log('server is running'))
